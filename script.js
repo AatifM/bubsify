d02ca15f4102191a576e0f6fbc333cff06a42b6f
AatifM <119080567+AatifM@users.noreply.github.com>
2025-05-07 17:58:02 -0400

    Making it github friendly

--- script.js
+++ script.js
@@ -8,65 +8,8 @@ const forwardBtn = document.querySelector("#forward");
 const forwardDiv = document.querySelector("#forward-container")
 const backwardBtn = document.querySelector("#backward");
 const backwardDiv = document.querySelector("#backward-container");
-const mikuIcon = document.querySelector("#mikuIcon");
-const caseIcon = document.querySelector("#caseIcon");
 const songImg = document.querySelector("#song-img");
 const songTitle = document.querySelector("#song-title");
-
-//This is gonna be really rough
-const miku = [
-    "songs/miku/[60fps Full] PoPiPo ぽっぴっぽー - Hatsune Miku 初音ミク DIVA Dreamy theater ドリーミーシアター English Romaji [TNf3GPizM58].mp3",
-    "songs/miku/[60fps Full] カンタレラ Cantarella - Hatsune Miku KAITO 初音ミク カイト Project DIVA English lyrics Romaji PDA [H0EtICId6mI].mp3",
-    "songs/miku/[60fps Full風] Magnet マグネット-Hatsune Miku Megurine Luka 初音ミク 巡音ルカ DIVA English lyrics romaji subtitles [vSnKX7kAgIc].mp3",
-    "songs/miku/[60fps Full風] Sweet Devil - Hatsune Miku 初音ミク Project DIVA Arcade English lyrics Romaji subtitles [Gu0luN8SrIs].mp3",
-    "songs/miku/[60fps Full風] The disappearance of Hatsune Miku -DEAD END-初音ミクの消失 DIVA Dreamy theater English Romaji [5qkTpJAhywg].mp3",
-    "songs/miku/[60fps Full風]Butterfly on My(Your) Right Shoulder 右肩の蝶 -Kagamine Len Rin 鏡音レン リン DIVA English romaji [hyV4qGAPKac].mp3",
-    "songs/miku/[60fps short] Clover Club - クローバークラブ Hatsune Miku 初音ミク Project DIVA Dreamy theater English Romaji [m_9pF4NpN6o].mp3",
-    "songs/miku/[1080P Full] Weekender Girl ウィークエンダーガール - Hatsune Miku 初音ミク DIVA English lyrics Romaji subtitles [vpYkz5WU1Vg].mp3",
-    "songs/miku/[1080P Full風] Romeo and Cinderella ロミオとシンデレラ -Hatsune Miku 初音ミク English lyrics romaji subtitles [naQjypGoOHY].mp3",
-    "songs/miku/[1080P Full風] World is Mine ワールドイズマイン -Hatsune Miku 初音ミク Project DIVA English lyrics Romaji PDFT [NY__VTIUsiU].mp3",
-    "songs/miku/[1080P Full風] 千本桜 Senbonzakura ＂One Thousand Cherry Trees＂- 初音ミク Hatsune Miku DIVA English Romaji [Mqps4anhz0Q].mp3",
-    "songs/miku/[Hatsune Miku] AaAaAaAAaAaAAa あぁあぁあぁああぁあぁああぁ PV (English Subtitles) [59jHEoNSlA0].mp3",
-    "songs/miku/[Hatsune Miku] Sharing The World by BIGHEAD feat.Hatsune Miku [MIKU EXPO] [GRc_Vz6kEjc].mp3",
-    "songs/miku/GHPZ - Hatsune Miku [V3 Eng] - Glass Wall [MJQ Retune] [HPzDehcMa7g].mp3",
-    "songs/miku/Hatsune Miku   Ievan Polkka Dance Comparison [DTSkygD1wR0].mp3",
-    "songs/miku/Hatsune Miku Project Diva 2nd Opening Full [80mPRUHJ0kc].mp3",
-    "songs/miku/Miku hatsune story rider (with lyrics) [QGhhs8xwxc8].mp3"
-];
-
-const caseStudy = [
-    "songs/caseStudy/ARE YOU OK？ [Bqh1krcUf6Q].mp3",
-    "songs/caseStudy/COMPLEXITIES [bDDtP_03GM0].mp3",
-    "songs/caseStudy/CYANIDE [Jnm3ukzmJe4].mp3",
-    "songs/caseStudy/ENTROPY [CLx631jSEug].mp3",
-    "songs/caseStudy/FRONTAL LOBE MUZIK (feat. Pharrell Williams) [GSpzscLeMuc].mp3",
-    "songs/caseStudy/OPEN UP [XAP-fzjXR_A].mp3",
-    "songs/caseStudy/RESTORE THE FEELING (feat. Sean Leon & Jacob Collier) [7CZmBMZclTY].mp3",
-    "songs/caseStudy/SUPERPOSITION (feat. John Mayer) [LV_V4Rsd9eU].mp3",
-    "songs/caseStudy/TOO DEEP TO TURN BACK [J5r3MDJGoaw].mp3"
-];
-
-const mikuImgs = [
-    "thumbnails/mikuImgs/PoPiPo.jpg",
-    "thumbnails/mikuImgs/Cantarella.jpg",
-    "thumbnails/mikuImgs/magnet.jpg",
-    "thumbnails/mikuImgs/sweetDevil.jpg",
-    "thumbnails/mikuImgs/disappearance.jpg",
-    "thumbnails/mikuImgs/butterflyOnMyShoulder.jpg",
-    "thumbnails/mikuImgs/CloverClub.jpg",
-    "thumbnails/mikuImgs/weekender.jpg",
-    "thumbnails/mikuImgs/Romeo&Cinderella.jpg",
-    "thumbnails/mikuImgs/worldIsMine.jpg",
-    "thumbnails/mikuImgs/OneThousand.jpg",
-    "thumbnails/mikuImgs/aaaaaa.jpg",
-    "thumbnails/mikuImgs/SharingTheWorld.jpg",
-    "thumbnails/mikuImgs/GHPZ.jpg",
-    "thumbnails/mikuImgs/ievanPolka.jpg",
-    "thumbnails/mikuImgs/ProjectDiva2.jpg",
-    "thumbnails/mikuImgs/storyRider.jpg"
-]
-
-const caseImgs = ["thumbnails/caseStudyImgs/caseStudyImg.jpg"];
 
 song.onloadedmetadata = function () {
     progress.max = song.duration;
@@ -85,20 +28,20 @@ playDiv.addEventListener("click", () => {
     }
 });
 
-mikuIcon.addEventListener("click", () => {
-    song.src = miku[0];
-    songImg.src = mikuImgs[0];
-    songTitle.textContent = decodeURIComponent(song.src.split("/").pop());
+.addEventListener("click", () => {
+    song.src = 
+    songImg.src = 
+    //songTitle.textContent = decodeURIComponent(song.src.split("/").pop());
     song.load();
     song.play();
     ctrlIcon.classList.remove("fa-play");
     ctrlIcon.classList.add("fa-pause");
 });
 
-caseIcon.addEventListener("click", () => {
-    song.src = caseStudy[0];
-    songImg.src = caseImgs[0];
-    songTitle.textContent = decodeURIComponent(song.src.split("/").pop());
+.addEventListener("click", () => {
+    song.src = 
+    songImg.src = 
+    //songTitle.textContent = decodeURIComponent(song.src.split("/").pop());
     song.load();
     song.play();
     ctrlIcon.classList.remove("fa-play");
@@ -118,19 +61,16 @@ volume.addEventListener("input", (e) => {
 });
 
 forwardDiv.addEventListener("click", () => {
-    if (song.src === "songs/Love You ⧸ Capchii feat. 初音ミク [i1umA0qoLa4].mp3") {
-        return;
-    }
-
     const currentSrc = song.src;
+    console.log(currentSrc);
     let currentFolder = decodeURIComponent(currentSrc.split("/")[10]);
     let currentImgs;
-    if (currentFolder === "miku") {
-        currentFolder = miku;
-        currentImgs = mikuImgs;
-    } else if (currentFolder === "caseStudy") {
-        currentFolder = caseStudy;
-        currentImgs = caseImgs;
+    if (currentFolder === "") {
+        currentFolder = 
+        currentImgs = 
+    } else if (currentFolder === "") {
+        currentFolder = 
+        currentImgs = 
     }
     const currentFile = decodeURIComponent(currentSrc.split("/").pop());
 
@@ -145,7 +85,7 @@ forwardDiv.addEventListener("click", () => {
 
     song.src = currentFolder[nextIndex];
     songTitle.textContent = decodeURIComponent(song.src.split("/").pop());
-    if (currentImgs === caseImgs) {songImg.src = currentImgs[0];}
+    if (currentImgs === ) {songImg.src = currentImgs[0];}
     else {songImg.src = currentImgs[nextIndex];}
     song.load();
     song.play();
@@ -155,19 +95,15 @@ forwardDiv.addEventListener("click", () => {
 });
 
 backwardDiv.addEventListener("click", () => {
-    if (song.src === "songs/Love You ⧸ Capchii feat. 初音ミク [i1umA0qoLa4].mp3") {
-        return;
-    }
-
     const currentSrc = song.src;
     let currentFolder = decodeURIComponent(currentSrc.split("/")[10]);
     let currentImgs;
-    if (currentFolder === "miku") {
-        currentFolder = miku;
-        currentImgs = mikuImgs;
-    } else if (currentFolder === "caseStudy") {
-        currentFolder = caseStudy;
-        currentImgs = caseImgs;
+    if (currentFolder === "") {
+        currentFolder = 
+        currentImgs = 
+    } else if (currentFolder === "") {
+        currentFolder = 
+        currentImgs = 
     }
     const currentFile = decodeURIComponent(currentSrc.split("/").pop());
 
@@ -175,14 +111,14 @@ backwardDiv.addEventListener("click", () => {
     let nextIndex;
 
     if (currentIndex === -1) {
-        return
+        return;
     } else {
         nextIndex = (currentIndex - 1 + currentFolder.length) % currentFolder.length;
     }
 
     song.src = currentFolder[nextIndex];
     songTitle.textContent = decodeURIComponent(song.src.split("/").pop());
-    if (currentImgs === caseImgs) {songImg.src = currentImgs[0];} 
+    if (currentImgs === ) {songImg.src = currentImgs[0];} 
     else {songImg.src = currentImgs[nextIndex];}
     song.load();
     song.play();
