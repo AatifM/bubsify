179adf1b3ddd06671557074255b2b823326404ab
AatifM <119080567+AatifM@users.noreply.github.com>
2025-05-07 18:18:07 -0400

    Added lofi music

--- script.js
+++ script.js
@@ -11,6 +11,46 @@ const backwardDiv = document.querySelector("#backward-container");
 const songImg = document.querySelector("#song-img");
 const songTitle = document.querySelector("#song-title");
 
+const lofiSongs = [
+    "songs/lofi/01 - [no copyright music] 'In Dreamland ' background music.mp3",
+    "songs/lofi/03 - [no copyright music] '2_00 AM' cute background music.mp3",
+    "songs/lofi/04 - [no copyright music] 'Taiyaki' cute background music.mp3",
+    "songs/lofi/05 - [no copyright music] 'Gameplay' cute background music.mp3",
+    "songs/lofi/06 - [no copyright music] 'Purple' lofi background music.mp3",
+    "songs/lofi/13 - [no copyright music] 'Loading' cute background music.mp3",
+    "songs/lofi/18 - [no copyright music] 'Dreamy Mode' cute background music.mp3",
+    "songs/lofi/23 - [no copyright music] 'One Thing' cute background music.mp3",
+    "songs/lofi/38 - [no copyright music] 'On The Top' lofi background music.mp3",
+    "songs/lofi/43 - [no copyright music] 'Feeling Cozy ' lofi background music.mp3",
+    "songs/lofi/54 - [no copyright music] 'Tomato farm' lofi background music.mp3",
+    "songs/lofi/55 - [no copyright music] 'Dango' lofi background music.mp3",
+    "songs/lofi/56 - [no copyright music] 'Wonder' cute lofi background music.mp3",
+    "songs/lofi/57 - [no copyright music] 'Breakfast' lofi background music.mp3",
+    "songs/lofi/58 - [no copyright music] 'Lovely' lofi background music.mp3",
+    "songs/lofi/59 - [no copyright music] 'little break' lofi background music.mp3",
+    "songs/lofi/60 - Space Aquarium - Lofi Study & Relaxation Music for Deep Focus.mp3"
+];
+
+const lofiThumbnails = [
+    "songs/lofi/01 - [no copyright music] 'In Dreamland ' background music.webp",
+    "songs/lofi/03 - [no copyright music] '2_00 AM' cute background music.webp",
+    "songs/lofi/04 - [no copyright music] 'Taiyaki' cute background music.webp",
+    "songs/lofi/05 - [no copyright music] 'Gameplay' cute background music.webp",
+    "songs/lofi/06 - [no copyright music] 'Purple' lofi background music.webp",
+    "songs/lofi/13 - [no copyright music] 'Loading' cute background music.webp",
+    "songs/lofi/18 - [no copyright music] 'Dreamy Mode' cute background music.webp",
+    "songs/lofi/23 - [no copyright music] 'One Thing' cute background music.webp",
+    "songs/lofi/38 - [no copyright music] 'On The Top' lofi background music.webp",
+    "songs/lofi/43 - [no copyright music] 'Feeling Cozy ' lofi background music.webp",
+    "songs/lofi/54 - [no copyright music] 'Tomato farm' lofi background music.webp",
+    "songs/lofi/55 - [no copyright music] 'Dango' lofi background music.webp",
+    "songs/lofi/56 - [no copyright music] 'Wonder' cute lofi background music.webp",
+    "songs/lofi/57 - [no copyright music] 'Breakfast' lofi background music.webp",
+    "songs/lofi/58 - [no copyright music] 'Lovely' lofi background music.webp",
+    "songs/lofi/59 - [no copyright music] 'little break' lofi background music.webp",
+    "songs/lofi/60 - Space Aquarium - Lofi Study & Relaxation Music for Deep Focus.webp"
+]
+
 song.onloadedmetadata = function () {
     progress.max = song.duration;
     progress.value = song.currentTime;
